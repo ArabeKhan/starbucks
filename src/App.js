d65831c2b1df1f36ea45af7528e1b6bd377f6c2c
Arabe Khane <khan.arabe016@gmail.com>
2022-10-14 22:11:34 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Learning React</p>
+        <h1>React is a Beauty</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
